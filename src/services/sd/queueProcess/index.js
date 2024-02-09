54df13dd5d9ce80d6685807d3c0a344be2a9618e
Your Name <gl060698@gmail.com>
2024-02-09 12:40:12 +0800

    换脸接口优化

--- src/services/sd/queueProcess/index.js
+++ src/services/sd/queueProcess/index.js
@@ -1,81 +1,128 @@
+import fs from 'fs/promises'; // 使用 promises 风格的 fs 模块
 import path from 'path';
 import prisma from '../../../db/prisma.js';
 import forwardToGPU from './forwardToGPU.js';
 
 // 工程根目录
-const projectRoot = path.resolve(new URL(import.meta.url).pathname, '../');
-// 图片目录
-const imagesDir = path.join(
-	projectRoot,
-	'sd_make_images',
-	new Date().toISOString().slice(0, 10)
-);
+const projectRoot = '/home/ubuntu/code/server/'; // 定义绝对路径
 
+// 检查并创建目录的函数
+async function ensureDirectoryExists(directory) {
+  try {
+    await fs.access(directory); // 检查目录是否存在
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      // 如果目录不存在，则创建目录
+      await fs.mkdir(directory, { recursive: true });
+    }
+  }
+}
 // 换脸接口
 export default async (req, res) => {
-	const { userId } = req.body;
-	const user = await prisma.user.findUnique({
-		where: { userId },
-	});
-	if (user.points < 1) {
-		return { error: 'no points' };
-	}
-	const gpuRes = await forwardToGPU(req.body);
-	// 调用成功
-	if (gpuRes?.data) {
-		const requestId = gpuRes.data.request_id;
-		// 保存图片到数据库，积分需要减1
-		updateUserImage(req.body, requestId);
-		updataUserInfo(userId);
-		return {
-			data: { requestId, status: 'pending', type: 'img2img' },
-		};
-	} else {
-		// 调用失败直接返回data为null
-		return { code: 'error', data: null };
-	}
+  const { userId } = req.body;
+  const user = await prisma.user.findUnique({
+    where: { userId },
+  });
+  if (user.points < 1) {
+    return { error: 'no points' };
+  }
+  const gpuRes = await forwardToGPU(req.body);
+  // 调用成功
+  if (gpuRes?.data) {
+    const requestId = gpuRes.data.request_id;
+    // 保存图片到文件并获取文件路径
+    // 图片目录
+    let imagesDir = path.join(
+      projectRoot,
+      'sd_make_images',
+      new Date().toISOString().slice(0, 10)
+    );
+    imagesDir = path.join(imagesDir, userId);
+    await ensureDirectoryExists(imagesDir);
+    const mainImagePath = await saveBase64Image(
+      req.body.init_images[0],
+      imagesDir
+    );
+    const roopImagePath = await saveBase64Image(
+      req.body.alwayson_scripts.roop.args[0],
+      imagesDir
+    );
+    // 保存图片路径到数据库
+    updateUserImage(userId, requestId, mainImagePath, roopImagePath);
+    // 积分需要减1
+    updataUserInfo(userId);
+    return {
+      data: { requestId, status: 'pending', type: 'img2img' },
+    };
+  } else {
+    // 调用失败直接返回data为null
+    return { code: 'error', data: null };
+  }
 };
-function updataUserInfo(userId) {
-	prisma.user.update({
-		where: {
-			userId,
-		},
-		data: {
-			points: {
-				// 使用 Prisma 提供的数学运算符对积分进行减一操作
-				decrement: 1,
-			},
-		},
-	});
+
+async function saveBase64Image(base64Data, imagesDir) {
+  if (!base64Data) return null;
+  try {
+    const imageName = `${Date.now()}.png`;
+    const imagePath = path.join(imagesDir, imageName);
+
+    await fs.writeFile(
+      imagePath,
+      Buffer.from(base64Data.replace(/^data:image\/\w+;base64,/, ''), 'base64')
+    );
+    return imagePath;
+  } catch (error) {
+    console.error('Error saving base64 image:', error.message);
+    return null;
+  }
 }
-// 更新userProcessImageData表中的任务数据
-async function updateUserImage(reqBody, requestId) {
-	try {
-		const userId = reqBody.userId;
-		if (!userId) {
-			return;
-		}
+// 积分需要减1
+async function updataUserInfo(userId) {
+  await prisma.user.update({
+    where: {
+      userId,
+    },
+    data: {
+      points: {
+        // 使用 Prisma 提供的数学运算符对积分进行减一操作
+        decrement: 1,
+      },
+    },
+  });
+}
 
-		// 新建一条换脸任务的数据，保存数据到数据库
-		const userTask = await prisma.userProcessImageData.create({
-			data: {
-				userId: userId,
-				imageType: 'img2img',
-				requestId: requestId,
-				requestStatus: 'pending',
-				mainImagePath: reqBody.init_images[0] || null,
-				roopImagePath: reqBody.alwayson_scripts.roop.args[0] || null,
-			},
-		});
+async function updateUserImage(
+  userId,
+  requestId,
+  mainImagePath,
+  roopImagePath
+) {
+  try {
+    if (!userId) {
+      return;
+    }
 
-		// 如果保存成功
-		if (userTask.userId) {
-			return;
-		} else {
-		}
-	} catch (error) {
-		console.error('Error updating user info in SQL:', error.message);
-	} finally {
-		await prisma.$disconnect();
-	}
+    // 新建一条换脸任务的数据，保存数据到数据库
+    const userTask = await prisma.userProcessImageData.create({
+      data: {
+        userId: userId,
+        imageType: 'img2img',
+        requestId: requestId,
+        requestStatus: 'pending',
+        mainImagePath: mainImagePath || null,
+        roopImagePath: roopImagePath || null,
+      },
+    });
+
+    // 如果保存成功
+    if (userTask.userId) {
+      return;
+    } else {
+      // 处理保存失败的情况
+    }
+  } catch (error) {
+    console.error('Error updating user info in SQL:', error.message);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
