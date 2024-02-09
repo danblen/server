7e730ff23bf4d07004dcfb47afcbd44f88d86a73
Your Name <gl060698@gmail.com>
2024-02-09 12:37:09 +0800

    图片检索、上传

--- src/services/image/getAllImages/index.js
+++ src/services/image/getAllImages/index.js
@@ -1,70 +1,75 @@
-import path from 'path';
-import fs from 'fs';
+import path from "path";
+import fs from "fs";
 
-const staticDirectory = '/home/ubuntu/code/server/static/';
+const staticDirectory = "/home/ubuntu/code/server/static/";
 
 export default (req, res) => {
-	const targetDirectory = path.join(staticDirectory, 'allImages');
-	const albumsDirectory = path.join(targetDirectory, 'albums');
-	const tagsDirectory = path.join(targetDirectory, 'tags');
-	const activityTagsDirectory = path.join(targetDirectory, 'activity_tags');
+  const targetDirectory = path.join(staticDirectory, "allImages");
+  const albumsDirectory = path.join(targetDirectory, "albums");
+  const tagsDirectory = path.join(targetDirectory, "tags");
+  const activityTagsDirectory = path.join(targetDirectory, "activity_tags");
+  const bannerDirectory = path.join(targetDirectory, "banner"); // 添加 banner 目录
 
-	if (
-		fs.existsSync(albumsDirectory) &&
-		fs.lstatSync(albumsDirectory).isDirectory()
-	) {
-		const albums = {};
-		fs.readdirSync(albumsDirectory).forEach((person) => {
-			const personPath = path.join(albumsDirectory, person);
-			if (fs.lstatSync(personPath).isDirectory()) {
-				const personObject = { index: '', urls: [] };
-				fs.readdirSync(personPath).forEach((category) => {
-					const categoryPath = path.join(personPath, category);
-					if (fs.lstatSync(categoryPath).isDirectory()) {
-						fs.readdirSync(categoryPath).forEach((image) => {
-							if (image.toLowerCase().match(/\.(png|jpg|jpeg|gif|bmp)$/)) {
-								const imagePath = `https://facei.top/statics/allImages/albums/${person}/${category}/${image}`;
-								if (category === 'index') {
-									personObject.index = imagePath;
-								} else if (category === 'urls') {
-									personObject.urls.push(imagePath);
-								}
-							}
-						});
-					}
-				});
-				albums[person] = personObject;
-			}
-		});
-		const tagsImage = getImagePaths(
-			tagsDirectory,
-			'https://facei.top/statics/allImages/tags'
-		);
-		const activityTagsImage = getImagePaths(
-			activityTagsDirectory,
-			'https://facei.top/statics/allImages/activity_tags'
-		);
+  if (
+    fs.existsSync(albumsDirectory) &&
+    fs.lstatSync(albumsDirectory).isDirectory()
+  ) {
+    const albums = {};
+    fs.readdirSync(albumsDirectory).forEach((person) => {
+      const personPath = path.join(albumsDirectory, person);
+      if (fs.lstatSync(personPath).isDirectory()) {
+        const personObject = { index: "", urls: [] };
+        fs.readdirSync(personPath).forEach((category) => {
+          const categoryPath = path.join(personPath, category);
+          if (fs.lstatSync(categoryPath).isDirectory()) {
+            fs.readdirSync(categoryPath).forEach((image) => {
+              if (image.toLowerCase().match(/\.(png|jpg|jpeg|gif|bmp)$/)) {
+                const imagePath = `https://facei.top/statics/allImages/albums/${person}/${category}/${image}`;
+                if (category === "index") {
+                  personObject.index = imagePath;
+                } else if (category === "urls") {
+                  personObject.urls.push(imagePath);
+                }
+              }
+            });
+          }
+        });
+        albums[person] = personObject;
+      }
+    });
+    const tagsImage = getImagePaths(
+      tagsDirectory,
+      "https://facei.top/statics/allImages/tags"
+    );
+    const activityTagsImage = getImagePaths(
+      activityTagsDirectory,
+      "https://facei.top/statics/allImages/activity_tags"
+    );
+    const bannerImage = getImagePaths(
+      bannerDirectory,
+      "https://facei.top/statics/allImages/banner"
+    ); // 获取 banner 图片路径
 
-		return { data: { albums, tagsImage, activityTagsImage } };
-	} else {
-		return { error: 'Albums directory not found.' };
-	}
+    return { data: { albums, tagsImage, activityTagsImage, bannerImage } }; // 添加 banner 图片路径到返回数据中
+  } else {
+    return { error: "Albums directory not found." };
+  }
 };
 
 function getImagePaths(directory, baseUrl) {
-	const imagesByTag = {};
-	fs.readdirSync(directory).forEach((tag) => {
-		const tagPath = path.join(directory, tag);
-		if (fs.lstatSync(tagPath).isDirectory()) {
-			const tagImages = fs
-				.readdirSync(tagPath)
-				.filter((image) =>
-					image.toLowerCase().match(/\.(png|jpg|jpeg|gif|bmp)$/)
-				)
-				.map((image) => `${baseUrl}/${tag}/${image}`);
+  const imagesByTag = {};
+  fs.readdirSync(directory).forEach((tag) => {
+    const tagPath = path.join(directory, tag);
+    if (fs.lstatSync(tagPath).isDirectory()) {
+      const tagImages = fs
+        .readdirSync(tagPath)
+        .filter((image) =>
+          image.toLowerCase().match(/\.(png|jpg|jpeg|gif|bmp)$/)
+        )
+        .map((image) => `${baseUrl}/${tag}/${image}`);
 
-			imagesByTag[tag] = tagImages;
-		}
-	});
-	return imagesByTag;
+      imagesByTag[tag] = tagImages;
+    }
+  });
+  return imagesByTag;
 }
--- src/services/image/uploadImages/index.js
+++ src/services/image/uploadImages/index.js
@@ -1,44 +1,64 @@
 import prisma from "../../../db/prisma.js";
 import fs from "fs";
-import path from "path"; // 导入path模块
+import path from "path";
 
-// 确保保存图像的目录存在
-const uploadDirectory = "./uploads";
+const uploadDirectory = "/home/ubuntu/code/server/uploads"; // 定义绝对路径
+
+// 如果目录不存在，则创建目录
 if (!fs.existsSync(uploadDirectory)) {
-  fs.mkdirSync(uploadDirectory);
+  fs.mkdirSync(uploadDirectory, { recursive: true });
 }
 
-// 上传图片和保存到数据库
 export default async (req) => {
   try {
-    const { userId, image, tag } = req.body;
+    const { userId, image, tag, description } = req.body;
 
-    if (!userId || !image || !tag) {
-      throw new Error("Missing required parameters"); // 如果缺少必需参数，则抛出错误
+    // 检查必需的参数是否存在且为数组
+    if (!userId || !image || !Array.isArray(image) || !tag || !description) {
+      throw new Error("Missing required parameters or image is not an array");
     }
 
-    // 删除 base64 字符串中的前缀
-    const base64Data = image[0].replace(/^data:image\/\w+;base64,/, "");
-    // 解码 base64 字符串为二进制数据
-    const imageData = Buffer.from(base64Data, "base64");
-    // 保存到本地文件系统
-    const imagePath = path.join(uploadDirectory, `${userId}-${Date.now()}.png`); // 生成文件路径
-    fs.writeFileSync(imagePath, imageData); // 将图像数据写入文件
-    console.log(imagePath);
-    // 保存到数据库
-    await prisma.imageUserUpload.create({
-      data: {
-        userId,
-        userPicPath: imagePath,
-        contentType: "image/png", // 根据实际情况设置
-        tags: "userUpload" + "," + tag,
-      },
+    const imagePaths = []; // 用于保存图片路径的数组
+
+    // 遍历上传的所有图片数据
+    image.forEach((image, index) => {
+      if (!image) return;
+      // 删除 base64 字符串中的前缀
+      const base64Data = image.replace(/^data:image\/\w+;base64,/, "");
+      // 解码 base64 字符串为二进制数据
+      const imageData = Buffer.from(base64Data, "base64");
+      // 生成文件路径，使用描述作为文件名
+      const imagePath = path.join(
+        uploadDirectory,
+        `${description}-${index + 1}.png`
+      );
+      // 将图像数据写入文件
+      fs.writeFileSync(imagePath, imageData);
+      console.log(`Image ${index + 1} saved as:`, imagePath);
+      // 将图片路径添加到数组中
+      imagePaths.push(imagePath);
     });
+
+    // 保存到数据库
+    for (const imagePath of imagePaths) {
+      await prisma.imageUserUpload.create({
+        data: {
+          userId,
+          description: description,
+          userPicPath: imagePath,
+          contentType: "image/png", // 根据实际情况设置
+          tags: tag, // 将标签放入数组中
+        },
+      });
+    }
+
     return { data: "success" };
   } catch (error) {
-    console.error("Error saving image to local filesystem:", error); // 打印错误信息到控制台
-    return { error: error.message }; // 返回包含错误信息的对象
+    // 处理错误信息
+    console.error("Error saving image to local filesystem:", error);
+    return { error: error.message };
   } finally {
-    await prisma.$disconnect(); // 关闭数据库连接
+    // 关闭数据库连接等操作
+    await prisma.$disconnect();
   }
 };
