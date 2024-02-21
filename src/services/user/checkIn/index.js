a868b85d7636d6b7956579429c51ca475c164295
Your Name <gl060698@gmail.com>
2024-02-22 00:36:45 +0800

    修复签到异常

--- src/services/user/checkIn/index.js
+++ src/services/user/checkIn/index.js
@@ -1,8 +1,8 @@
+import { DateTime } from 'luxon';
 import prisma from '../../../db/prisma.js';
 
 export default async (req, res) => {
   const { userId } = req.body;
-  const today = new Date().toISOString().split('T')[0]; // 获取当前日期（格式：YYYY-MM-DD）
   const user = await prisma.user.findUnique({
     where: {
       userId,
@@ -12,22 +12,23 @@ export default async (req, res) => {
   if (!user) {
     return null; // 用户不存在
   }
-
+  const today = DateTime.now().setZone('Asia/Shanghai');
+  //先将时间字符串转换成时间格式
+  const lastCheckInDate = DateTime.fromISO(user.lastCheckIn);
   // 如果上次签到日期不是今天，则进行签到
-  if (user.lastCheckIn !== today) {
+  if (!lastCheckInDate.hasSame(today, 'day')) {
     const updatedUser = await prisma.user.update({
       where: {
         userId,
       },
       data: {
         points: {
-          increment: 1,
+          increment: 2,
         },
         isChecked: true,
         lastCheckIn: today, // 更新上次签到日期
       },
     });
-
     return { data: updatedUser };
   } else {
     return { data: null }; // 今天已经签到过了
--- src/services/user/getUserInfo/index.js
+++ src/services/user/getUserInfo/index.js
@@ -1,14 +1,33 @@
+import { DateTime } from 'luxon';
 import prisma from '../../../db/prisma.js';
 
 export default async (req, res) => {
-	const user = await prisma.user.findUnique({
-		where: {
-			userId: req.body.userId,
-		},
-	});
-	if (!user) {
-		return { data: null };
-	} else {
-		return { data: user };
-	}
+  const user = await prisma.user.findUnique({
+    where: {
+      userId: req.body.userId,
+    },
+  });
+
+  if (!user) {
+    return { data: null };
+  } else {
+    const today = DateTime.now().setZone('Asia/Shanghai');
+    //先将时间字符串转换成时间格式
+    const lastCheckInDate = DateTime.fromISO(user.lastCheckIn);
+
+    if (!lastCheckInDate.hasSame(today, 'day')) {
+      let userSql;
+      userSql = await prisma.user.update({
+        where: {
+          userId: user.userId,
+        },
+        data: {
+          isChecked: false,
+        },
+      });
+      return { data: userSql };
+    }
+
+    return { data: user };
+  }
 };
